deb241e52d52fa9a6ae8a0059fe8a43420674172
okachynskyy <oleksiy.kachynskyy@gmail.com>
2016-07-04 11:13:21 +0300

    add ability to fetch data from api and display in view

--- app/components/forecast/forecast.component.js
+++ app/components/forecast/forecast.component.js
@@ -8,45 +8,17 @@ module.exports = {
 ForecastController.$inject = ['Forecast'];
 function ForecastController(Forecast) {
   var $ctrl = this;
+  $ctrl.cities = ['Lviv', 'Kyiv', 'Odessa'];
 
-  $ctrl.weatherConditions = [
-    {date: 'May 12, 03:00', condition: 'Rain'},
-    {date: 'May 12, 06:00', condition: 'Clouds'},
-    {date: 'May 12, 09:00', condition: 'Rain'},
-    {date: 'May 12, 12:00', condition: 'Rain'},
-    {date: 'May 12, 15:00', condition: 'Clouds'},
-    {date: 'May 12, 18:00', condition: 'Rain'},
-    {date: 'May 12, 21:00', condition: 'Rain'},
-    {date: 'May 13, 00:00', condition: 'Clouds'},
-    {date: 'May 13, 03:00', condition: 'Clouds'},
-    {date: 'May 13, 06:00', condition: 'Rain'}
-  ];
+  Forecast.fetchFiveForecast($ctrl.cities[0])
+    .then(function (data){
+      $ctrl.weatherData=data;
+  });
 
-  $ctrl.humidity = [
-    {date: 'May 12, 03:00', humidity: '94%'},
-    {date: 'May 12, 06:00', humidity: '96%'},
-    {date: 'May 12, 09:00', humidity: '88%'},
-    {date: 'May 12, 12:00', humidity: '83%'},
-    {date: 'May 12, 15:00', humidity: '77%'},
-    {date: 'May 12, 18:00', humidity: '100%'},
-    {date: 'May 12, 21:00', humidity: '98%'},
-    {date: 'May 13, 00:00', humidity: '94%'},
-    {date: 'May 13, 03:00', humidity: '93%'},
-    {date: 'May 13, 06:00', humidity: '100%'}
-  ];
-
-  $ctrl.windSpeed = [
-    {date: 'May 12, 03:00', speed: '1.51 m/s'},
-    {date: 'May 12, 06:00', speed: '0.86 m/s'},
-    {date: 'May 12, 09:00', speed: '1.51 m/s'},
-    {date: 'May 12, 12:00', speed: '0.86 m/s'},
-    {date: 'May 12, 15:00', speed: '1.51 m/s'},
-    {date: 'May 12, 18:00', speed: '0.86 m/s'},
-    {date: 'May 12, 21:00', speed: '1.51 m/s'},
-    {date: 'May 13, 00:00', speed: '0.86 m/s'},
-    {date: 'May 13, 03:00', speed: '1.51 m/s'},
-    {date: 'May 13, 06:00', speed: '0.86 m/s'}
-  ];
-
-
+  $ctrl.changeCity = function (city) {
+    Forecast.fetchFiveForecast(city)
+      .then(function (data){
+        $ctrl.weatherData=data;
+    });
+  };
 }
--- app/components/forecast/forecast.factory.js
+++ app/components/forecast/forecast.factory.js
@@ -2,12 +2,45 @@ module.exports = ForecastFactory;
 
 ForecastFactory.$inject = ['$http'];
 function ForecastFactory($http) {
-  return {
-    test: test
-  };
+  var API_URL = "http://api.openweathermap.org/data/2.5/forecast";
+  var APPID = "974cf989f33a668473fd4d704c86c5e8";
 
-  function test() {
-    return 'Test function from ForecastFactory';
+  function fetchFiveForecast (city) {
+    return $http({
+      url: API_URL, 
+      method: "GET",
+      params: {q: city, units: 'metric', APPID: APPID, cnt: 10}
+    })
+    .then(function(response) {
+      var data = response.data;
+      var weatherData = {
+        weatherConditions: [],
+        humidity: [],
+        windSpeed: []
+      };
+      for(var i = 0; i< data.list.length; i++){
+        var formatDate = new Date(data.list[i].dt*1000).toLocaleTimeString("en-us", {
+          month: "short", day: 'numeric', hour: '2-digit', minute:'2-digit', hour12: false
+        });
+        weatherData.weatherConditions.push({
+          date: formatDate, 
+          condition: data.list[i].weather[0].main
+        });
+        weatherData.humidity.push({
+          date: formatDate, 
+          humidity: data.list[i].main.humidity
+        });
+        weatherData.windSpeed.push({
+          date: formatDate, 
+          speed: data.list[i].wind.speed
+        });
+      }
+      return weatherData;
+    });
   }
+
+  return {
+    fetchFiveForecast: fetchFiveForecast
+  };
 
 }
