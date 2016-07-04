7c592a6aeebbaf6779f302c18444d5fa5bb32a4c
okachynskyy <oleksiy.kachynskyy@gmail.com>
2016-07-04 04:10:36 +0300

    add basic container and navbar component

--- app/components/index.js
+++ app/components/index.js
@@ -2,11 +2,13 @@ var angular = require('angular');
 var ngRoute = require('angular-route');
 
 var forecast = require('./forecast');
+var navbar = require('./navbar');
 
 
 module.exports = angular.module('angularWeatherClient', [
   ngRoute,
-  forecast
+  forecast,
+  navbar.name
 ]).
 config(RouteConfig);
 
