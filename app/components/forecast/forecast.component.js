4acccc3c654d5dfff40cf69edca2c66e8125b9b8
okachynskyy <oleksiy.kachynskyy@gmail.com>
2016-07-04 07:22:08 +0300

    add less compiler and basic view with mocked data

--- app/components/forecast/forecast.component.js
+++ app/components/forecast/forecast.component.js
@@ -9,10 +9,44 @@ ForecastController.$inject = ['Forecast'];
 function ForecastController(Forecast) {
   var $ctrl = this;
 
-  $ctrl.test = test;
+  $ctrl.weatherConditions = [
+    {date: 'May 12, 03:00', condition: 'Rain'},
+    {date: 'May 12, 06:00', condition: 'Clouds'},
+    {date: 'May 12, 09:00', condition: 'Rain'},
+    {date: 'May 12, 12:00', condition: 'Rain'},
+    {date: 'May 12, 15:00', condition: 'Clouds'},
+    {date: 'May 12, 18:00', condition: 'Rain'},
+    {date: 'May 12, 21:00', condition: 'Rain'},
+    {date: 'May 13, 00:00', condition: 'Clouds'},
+    {date: 'May 13, 03:00', condition: 'Clouds'},
+    {date: 'May 13, 06:00', condition: 'Rain'}
+  ];
 
-  function test() {
-    return Forecast.test();
-  }
+  $ctrl.humidity = [
+    {date: 'May 12, 03:00', humidity: '94%'},
+    {date: 'May 12, 06:00', humidity: '96%'},
+    {date: 'May 12, 09:00', humidity: '88%'},
+    {date: 'May 12, 12:00', humidity: '83%'},
+    {date: 'May 12, 15:00', humidity: '77%'},
+    {date: 'May 12, 18:00', humidity: '100%'},
+    {date: 'May 12, 21:00', humidity: '98%'},
+    {date: 'May 13, 00:00', humidity: '94%'},
+    {date: 'May 13, 03:00', humidity: '93%'},
+    {date: 'May 13, 06:00', humidity: '100%'}
+  ];
+
+  $ctrl.windSpeed = [
+    {date: 'May 12, 03:00', speed: '1.51 m/s'},
+    {date: 'May 12, 06:00', speed: '0.86 m/s'},
+    {date: 'May 12, 09:00', speed: '1.51 m/s'},
+    {date: 'May 12, 12:00', speed: '0.86 m/s'},
+    {date: 'May 12, 15:00', speed: '1.51 m/s'},
+    {date: 'May 12, 18:00', speed: '0.86 m/s'},
+    {date: 'May 12, 21:00', speed: '1.51 m/s'},
+    {date: 'May 13, 00:00', speed: '0.86 m/s'},
+    {date: 'May 13, 03:00', speed: '1.51 m/s'},
+    {date: 'May 13, 06:00', speed: '0.86 m/s'}
+  ];
+
 
 }
